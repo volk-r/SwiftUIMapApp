7be82c8d3f0f6175ddc0017e0a45a6961669de62
Roman Romanov <rromanov.vk@gmail.com>
2023-12-27 22:45:00 +0500

    SwiftUI Map App: Project Setup with Assets and Custom Data Model

---
+++ Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftUIMapApp
+//
+//  Created by Roman Romanov on 27.12.2023.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
