bdeda8b4fdf82ef9da7d74ce17535715c401d5e3
Roman Romanov <rromanov.vk@gmail.com>
2024-01-01 20:45:16 +0500

    SwiftUI Map App #5 Location Preview cards with asymmetric Transitions

--- SwiftUIMapApp/ViewModels/LocationsViewModel.swift
+++ SwiftUIMapApp/ViewModels/LocationsViewModel.swift
@@ -57,4 +57,26 @@ class LocationsViewModel: ObservableObject {
         }
     }
     
+    func nextButtonPressed() {
+        // Get the current index
+        guard let currentIndex = locations.firstIndex(where: { $0 == mapLocation }) else {
+            print("Could not find current index in locations array! Should never happen.")
+            return
+        }
+        
+        // Check if nextIndex is valid
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            // Next index is NOT valid
+            // restart from 0
+            guard let firstLocation = locations.first else { return }
+            showNextLocation(location: firstLocation)
+            return
+        }
+        
+        // Next location IS valid
+        let nextLocation = locations[nextIndex]
+        showNextLocation(location: nextLocation)
+    }
+    
 }
